44172dd337039ef744fe5b65ee72064d3fb8bbba
Ayushhad <108228706+Ayushhad@users.noreply.github.com>
2022-10-26 12:39:30 +0530

    Update ls.c

--- ls.c
+++ ls.c
@@ -37,7 +37,7 @@ int main(int argc,char *argv[]){
                 printf("%lld %s\t",x->d_ino,y);
             }
         }
-        print("\n");
+        printf("\n");
     }
     else if(!strcmp(argv[1],"-l")){
         while((x=readdir(dir))!=NULL){

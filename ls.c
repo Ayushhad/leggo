ae5243249b855f806f8b17ecca441a3d3ed7803b
Ayushhad <108228706+Ayushhad@users.noreply.github.com>
2022-10-26 12:37:46 +0530

    Update ls.c

--- ls.c
+++ ls.c
@@ -1,7 +1,12 @@
+#include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
-#include <dirent.h>
+#include <pthread.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <errno.h>
 
 int main(int argc,char *argv[]){
     
